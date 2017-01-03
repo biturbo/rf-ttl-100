b6acca732b9d15fbcfb04d0e583aa99c156c0acd
lbaumgaertner <lbaumgaertner@mathematik.uni-marburg.de>
2017-01-03 13:44:50 +0100

    - echoservice prints timestamp and date to local console for logging purposes

--- ttl-100-echoservice.py
+++ ttl-100-echoservice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # echos messages in the form of "echo [msg]\n"
 
-import ttl100, time, sys
+import ttl100, time, sys, datetime
 port = ttl100.get_serial_port()
 
 time.sleep(1)
@@ -9,12 +9,12 @@ while True:
     line = ""
     while len(line) == 0 or line[-1] != "\n":
         line += port.read(1)
-    sys.stdout.write(line)
+    sys.stdout.write(str(datetime.datetime.now()) + " " + line)
     sys.stdout.flush()
     
     if line.startswith("echo "):
         ohce = "ohce "+line[5:]
         port.write(ohce)
-        sys.stdout.write(ohce)
+        sys.stdout.write(str(datetime.datetime.now()) + " " + ohce)
         sys.stdout.flush()
         
